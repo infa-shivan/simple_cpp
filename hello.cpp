aee00998d6e222abcebde4304248804e73c68449
infa-shivan <shivan@informatica.com>
2021-05-20 13:02:37 +0530

    vulnerable inject

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,24 @@
-#include <iostream>
+# Alerts due to an unbounded write.
+# See https://github.com/github/codeql/blob/main/cpp/ql/src/Security/CWE/CWE-120/UnboundedWrite.ql
+
 #include <stdio.h>
-// using namespace std;
-  
-int main()  
-{  
-    printf("%s","Hello, world!\n");
-    return 0;
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+       char buffer[5];
+
+       if (argc < 2)
+       {
+              printf("strcpy() NOT executed....\n");
+              printf("Syntax: %s <characters>\n", argv[0]);
+              exit(0);
+       }
+
+       strcpy(buffer, argv[1]);
+       printf("buffer content= %s\n", buffer);
+       printf("strcpy() executed...\n");
+
+       return 0;
 }
